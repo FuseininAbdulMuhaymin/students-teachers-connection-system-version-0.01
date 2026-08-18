7aab37b17f3e640c61777c203a805eb9bcf83daf
abdulmuhayminf@gmail.com <abdulmuhayminf@gmail.com>
2026-08-18 12:45:15 +0000

    tried a new approach to solve the login of teachers but sill not workin

--- services/auth_teacher.py
+++ services/auth_teacher.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from auth.security import hash_password
-from  crud.teacher import (create_teacher,get_teacher_by_email,get_teacher_username)
+from  crud.teacher import (create_teacher,get_teacher_by_email,get_teacher_by_username)
 from fastapi import HTTPException, status
 from auth.security import(verify_password,create_access_token)
 
@@ -10,8 +10,8 @@ def register_teacher(db:Session,username:str,email:str,password:str,teacher):
     existing_email = get_teacher_by_email(db,email)
     if existing_email:
         raise HTTPException(status_code= status.HTTP_400_BAD_REQUEST,detail="Email alreaddy registered")
-    #checking  username
-    existing_username = get_teacher_username(db,username)
+    #checking_username
+    existing_username = get_teacher_by_username(db,username)
     if existing_username:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,details="Username already takenmko")
 
@@ -29,17 +29,31 @@ def register_teacher(db:Session,username:str,email:str,password:str,teacher):
     return teacher
 
 #Loging  as teacher 
-def logging_teacher(db:Session,username:str,email:str,password:str,teacher)
-    existing_email = get_teacher_by_email(db,email)
-    if email != existing_email:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+def authentication_teacher(db:Session,username:str,password:str):
+    #Find the teacher in the database
+    teacher = get_teacher_by_username(db,username)
     
-hashes_password = hash_password(password) 
-
-teacher = create_teacher(
-    db = db,
-    username = username,
-    email=email,
-    hash_password = hash_password
-)
- return teeacher
+    #if the  teacher doesn't exists
+    if not teacher:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid username or password")
+    
+    #check if the password provided is the  correct or 
+    if not verify_password(password,teacher.hashed_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid pasword")
+    
+    #Create jwt after successfully authencticate the teacher
+    access_token = create_access_token(data={"sub":str(teacher.id)})
+    return {
+        "access_token":access_token,
+        "token_type":"bearer"
+    }
+    
+# so this the sequence am using here 
+# Find user
+#    ↓
+# Verify password
+#    ↓
+# Create JWT
+#    ↓
+# Return JWT
+#####-------------- THE END OF AUTHENTICATING  A TEACHER -----######
