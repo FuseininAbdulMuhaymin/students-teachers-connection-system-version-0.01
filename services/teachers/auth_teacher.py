35a6b1ce6e0afbcff963f4fbb727c2280ecd4ed1
abdulmuhayminf@gmail.com <abdulmuhayminf@gmail.com>
2026-09-06 22:26:15 +0000

    fixed an expected error

--- services/teachers/auth_teacher.py
+++ services/teachers/auth_teacher.py
@@ -4,9 +4,10 @@ from datetime import timedelta
 from  auth.security import  hash_password
 from  crud import teacher as teacher_crud
 from  crud import logging_teacher 
-from model import Teachers
+from models import Teachers
 from schemas.teacher import TeacherCreate,Login
-
+from crud import get_teacher_by_email
+from auth import verify_password, create_access_token
 
 def register_new_teacher(db:Session,teacher_in:TeacherCreate):
     if teacher_crud.get_teacher_by_email(db,email = teacher_in.email):
@@ -31,5 +32,31 @@ def register_new_teacher(db:Session,teacher_in:TeacherCreate):
         hashed_password = hashed_password
     )
     
-def log_teacher_in(db:Session,teacher_log:Login):
-    if logging_teacher(db)
+def login_teacher(db: Session, email: str, password: str):
+
+    # 1. Find teacher
+    teacher = get_teacher_by_email(db=db,email=email)
+
+    # 2. Check if teacher exists
+    if not teacher:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid email or password")
+
+    # 3. Verify password
+    password_is_valid = verify_password(password,teacher.hashed_password)
+
+    if not password_is_valid:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid email or password"
+        )
+
+    # 4. Create JWT
+    access_token = create_access_token(
+        data={"sub": str(teacher.id)}
+    )
+
+    # 5. Return token
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
